5ac481c18a819d0d8921259bb552e5870767ffc1
Martijn <127121@student.drenthecollege.nl>
2019-12-12 11:29:11 +0100

    added comments

--- src/Opdracht1.java
+++ src/Opdracht1.java
@@ -5,12 +5,14 @@ public class Opdracht1 {
     }
 
     public static void opdracht1() {
+        //define variables
         int n1 = 0;
         int n2 = 1;
         int n3, i;
         int count = 10;
         System.out.print(n1 + " " + n2);
 
+        //calculation
         for(i = 2; i < count; ++i)
         {
             n3 = n1 + n2;
@@ -18,6 +20,5 @@ public class Opdracht1 {
             n1 = n2;
             n2 = n3;
         }
-
     }
 }
