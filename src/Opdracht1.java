89fd734f6c0c1b4706ac0cb8344712c19de9d736
Martijn <127121@student.drenthecollege.nl>
2019-12-18 11:47:06 +0100

    new switch

--- src/Opdracht1.java
+++ src/Opdracht1.java
@@ -14,7 +14,7 @@ public class Opdracht1 {
         result = Calculation.calc(equation);
         System.out.println(equation + " = " + result);
 
-        equation = "(8 + 1) - 3 + (2 * 3)";
+        equation = "(8 + 1) / 3 + (2 * 3)";
         result = Calculation.calc(equation);
         System.out.println(equation + " = " + result);
 
@@ -35,14 +35,38 @@ public class Opdracht1 {
          System.out.println("komt binnen:" + equation + " lengte string:" + equation.length());
 
         //1. splits getallen en operators/tekens (ontleden)
-        String Array[] = equation.split(" ");
+         //String Array[] = equation.split(" ");
         //2. string naar getallen omzetten
-          int n1 = Integer.parseInt(Array[0]);
-          int n2 = Integer.parseInt(Array[2]);
-          System.out.println(n1);
-          System.out.println(Array[1]);
-          System.out.println(n2);
+          //int n1 = Integer.parseInt(Array[0]);
+          //werkt niet: int n3 = Integer.parseInt(Array[1]);
+          //int n2 = Integer.parseInt(Array[2]);
 
-        return 0;
+
+          char[] array = equation.toCharArray();
+
+          //generaliseer omdat we niet weten wat we op welke positie  in equation aantreffen
+
+         for(int i=0; i<array.length; i++){
+             if (Character.isDigit(equation.charAt(i))) {
+                 System.out.println(equation.charAt(i) + " is a digit");
+             } else {
+                 switch(equation.charAt(i)) {
+                     case '+':
+                         System.out.println("is a +");
+                         break;
+                     case '-':
+                         System.out.println("is a -");
+                         break;
+                     case '*':
+                         System.out.println("is a *");
+                         break;
+                     case '/':
+                         System.out.println("is a /");
+                         break;
+                     default:
+                 }
+             }
+         }
+          return 0;
       }
   }
